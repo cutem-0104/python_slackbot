9c7a3580701de7211c305d64a8b6f5d9f0ba1772
shuhei.sugaya <shuhei.sugaya>
2018-04-08 15:51:30 +0900

    commit

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,9 +1,8 @@
 # coding: utf-8
-import setting
-
+import os
 
 # botアカウントのトークンを指定
-API_TOKEN = setting.API_TOKEN
+API_TOKEN = os.environ["API_TOKEN"]
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "は？"
