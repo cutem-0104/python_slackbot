f950d798c7e5837f5520ee20f64f71a31739f043
shuhei.sugaya <shuhei.sugaya>
2018-04-08 15:29:38 +0900

    first commit

---
+++ slackbot_settings.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import setting
+
+
+# botアカウントのトークンを指定
+API_TOKEN = setting.API_TOKEN
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "は？"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
